c85c8f706e1afb7bb2429fa18b4b56976412d4b3
codewithzaman <naderzaman50@gmail.com>
2024-01-16 22:42:54 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const name = 'pedro';
+  const name:string = 'pedro';
+  const age:number = 35;
+  const isMarriad:boolean = true;
   return (
     <div className="App">
       {name}
+      {' '}
+      {age}
+      {isMarriad}
     </div>
   );
 }
